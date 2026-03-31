766a77173c9e0219473fbb0ad84f1214cb9f8b16
Sam Tucker-Davis <126325182+stuckvgn@users.noreply.github.com>
2026-03-31 14:18:58 +0530

    customize: rename footer website link to Open Paws Website

---
+++ customize/translations/messages.js
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Open Paws
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+define(function () {
+    var Messages = {};
+    Messages.footer_website = "Open Paws Website";
+    return Messages;
+});
